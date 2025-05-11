b199637d5e3deb8e92b66c9f96fe9649425e1092
samuel <samuelzuuka@gmail.com>
2025-05-11 19:30:01 +0800

    modify ports

--- start_sd-webui-sdxl.sh
+++ start_sd-webui-sdxl.sh
@@ -1,1 +1,1 @@
-python launch.py --xformers --api --log-startup --api-log --no-half --disable-nan-check --listen --port 17861
+python launch.py --xformers --api --log-startup --api-log --no-half --disable-nan-check --listen --port 7860
